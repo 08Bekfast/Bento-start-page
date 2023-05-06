10fdaed1b6d23ddd2d41dc591beb61a3f314e1cd
08Bekfast <85724929+08Bekfast@users.noreply.github.com>
2023-05-06 16:04:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const CONFIG = {
 	// └─┘┴ ┴└─┘┴└─┘└─┘
 
 	// General
-	name: 'John',
+	name: 'Karthik',
 	imageBackground: false,
 	openInNewTab: true,
 	twelveHourFormat: false,
@@ -32,8 +32,8 @@ const CONFIG = {
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
 	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-	defaultLatitude: '37.775',
-	defaultLongitude: '-122.419',
+	defaultLatitude: '12.986327650325636',
+	defaultLongitude: '77.54854257340494',
 
 	// Autochange
 	autoChangeTheme: true,
